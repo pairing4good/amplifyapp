aa6ef101ee9db0b6c75b71c14329186ed421e586
dkessler <dkessler@pillartechnology.com>
2021-05-16 07:44:10 -0500

    composed app components for clarity and simplicity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { API } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import Header from './Header'
+import NoteForm from './NoteForm'
+import NoteList from './NoteList'
+import Footer from './Footer'
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <NoteForm formData={formData} 
+        setFormDataCallback={setFormData} 
+        createNoteCallback={createNote}/>
+      <NoteList notes={notes} 
+        deleteNoteCallback={deleteNote} />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
